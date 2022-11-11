8676008eb5c2d5705f1af5c902ac73de17ab78a6
Nidhi-hub-pixel <76686078+Nidhi-hub-pixel@users.noreply.github.com>
2022-11-11 16:34:33 +0530

    Add files via upload

---
+++ Automation.py
@@ -0,0 +1,17 @@
+from googlesearch import search
+results = search(term="Python Development Monterail", num_results=10, lang="en")
+results = list(results)
+print(results)
+
+[
+   "/services/python-development-services",
+   "/careers/python-developer-wroclaw",
+   "/blog/topic/python",
+   "/blog/what-its-like-to-be-a-python-developer-in-monterail",
+   "/blog/python-for-mobile-app-development",
+   "https://apply.workable.com/monterail/j/F5F19D831D/",
+   "https://bulldogjob.pl/companies/jobs/81190-senior-python-developer-wroclaw-monterail",
+   "https://pl.linkedin.com/in/artur-pietracha-b216b641",
+   "https://www.facebook.com/monterail",
+   "https://fi.talent.com/view?id=1abad4456af7",
+] 
